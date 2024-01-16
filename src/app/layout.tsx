8308f7dd1a233c2a8b14c7c1d29087a82685b22b
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-16 14:29:53 -0500

    Add Navbar component and font-awesome stylesheet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,30 @@
-import type { Metadata } from 'next'
+import type { Metadata } from 'next';
 
-import './globals.css'
-
-
+import './globals.css';
+import Navbar from './components/ui/Navbar';
 
 export const metadata: Metadata = {
   title: 'The Library App',
   description: 'Library Inventory App',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" data-theme='business'>
+    <html lang="en" data-theme="business">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        />
+      </head>
       <body>
+        <Navbar />
         {children}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
