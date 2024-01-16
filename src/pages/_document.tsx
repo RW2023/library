7b1374c21fecb7a0fc28a5f5bea1b920a1c0c079
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-16 14:14:43 -0500

    create _document.tsx and test fa link (did not work in another project trying another setup here)

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+// _document.tsx src/pages/_document.tsx
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
