f6c554411d6c006923c5a17ee256abfc7fc02efe
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-16 14:30:00 -0500

    Remove font-awesome stylesheet from _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,12 +5,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-          />
-        </Head>
+        {/* Head */}
         <body>
           <Main />
           <NextScript />
