ff7cbf925557f3e1f1e66ed949d50721b4158387
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-17 05:18:14 -0500

    Add genre and description to Book type and update BookList component

--- src/app/components/BookList.tsx
+++ src/app/components/BookList.tsx
@@ -4,12 +4,15 @@
 import React, { useState, useEffect } from 'react';
 import { supabase } from '@/utils/supabaseClient';
 import LoadingInline from '@/app/components/ui/LoadingInline';
+import SubHeading from '@/app/components/ui/SubHeading';
 
 // Define a type for the book
 type Book = {
   id: number;
   title: string;
   author: string;
+  genre: string;
+  description: string;
 };
 
 const BooksList: React.FC = () => {
@@ -19,9 +22,7 @@ const BooksList: React.FC = () => {
   useEffect(() => {
     const fetchBooks = async () => {
       try {
-        const { data, error } = await supabase
-          .from('books')
-          .select('*');
+        const { data, error } = await supabase.from('books').select('*');
 
         if (error) throw error;
 
@@ -36,21 +37,32 @@ const BooksList: React.FC = () => {
     fetchBooks();
   }, []);
 
-  if (isLoading) return <LoadingInline/>;
+  if (isLoading) return <LoadingInline />;
   return (
-    <div>
-      {books.length > 0 ? (
-        books.map((book) => (
-          <div key={book.id}>
-            <h3>{book.title}</h3>
-            <p>{book.author}</p>
-          </div>
-        ))
-      ) : (
-        <div>No books found.</div>
-      )}
+    <div className="container mx-auto p-4 bg-base-200 m-2 rounded ">
+      <div className="form-control">
+        {/* Assuming you have a form component here */}
+        {/* <YourFormComponent /> */}
+      </div>
+      <div className="divider"></div> {/* Divider line between form and list */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 border border-1 rounded-md bg-base-200">
+        {books.length > 0 ? (
+          books.map((book) => (
+            <div key={book.id} className="card bg-base-300 shadow-xl">
+              <div className="card-body">
+                <SubHeading title={` Title: ${book.title}`} />
+                <p>Author: {book.author}</p>
+                <p>Genre: {book.genre}</p>
+                <p>Description: {book.description}</p>
+                {/* You can add more book details here */}
+              </div>
+            </div>
+          ))
+        ) : (
+          <div className="text-center">No books found.</div>
+        )}
+      </div>
     </div>
   );
 };
-
 export default BooksList;
