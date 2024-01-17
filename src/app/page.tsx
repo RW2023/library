de0956bab565314b5b52e4c35d1a9a567de59f08
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-17 01:07:13 -0500

    Add hero section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,34 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import LightHeading from '@/app/components/ui/LightSubHeading';
+import LightSubHeading from '@/app/components/ui/LightSubHeading';
 
 export default function Home() {
   return (
-<main>
-  <div className='min-h-screen flex flex-col justify-center items-center text-4xl'>
-    <h1>Library App </h1>
-  </div>
-</main>
-  )
+    <main>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: 'url(hero.webp)',
+        }}
+      >
+        <div className="hero-overlay bg-opacity-70"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            {/* <LightHeading title="Welcome to the Library" /> */}
+            <h1
+              className="text-5xl"
+              style={{
+                textShadow:
+                  '1px 1px 0px #000, -1px -1px 0px #000, 1px -1px 0px #000, -1px 1px 0px #000',
+              }}
+            >
+            Enter the Library
+            </h1>
+
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
